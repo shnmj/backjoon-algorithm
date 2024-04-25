15bb2137729636530c8f6af625b4f4d41a008cd8
Shin Min Jae <163399297+shnmj@users.noreply.github.com>
2024-04-25 19:42:42 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String st  = sc.next();
+        int result = st.charAt(0);
+        
+        System.out.println(result);
+    }
+}
