626809dda7e6381dba5338f7cbe8e78be81fa9c5
Shin Min Jae <163399297+shnmj@users.noreply.github.com>
2024-05-27 14:14:30 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 196 ms, Memory: 17564 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -10,6 +10,11 @@ public class Main{
         int ans    = 1;
         
         for(int i=0; i<len/2; i++) {
+            
+            /* charAt() 사용 시 단어가 서로 일치해야 하므로 
+                str.charAt(i)와 str.charAt(len-1-i)가 다른경우
+                ans = 0이 되게 설정. */
+            
             if(str.charAt(i) != str.charAt(len-1-i)) {
                 ans = 0;
             }
