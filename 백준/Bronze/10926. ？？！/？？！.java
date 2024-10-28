240df4dc01fe797802e2a747ad07cd3a731b516a
Shin Min Jae <163399297+shnmj@users.noreply.github.com>
2024-10-29 01:34:23 +0900

    [Bronze V] Title: ??!, Time: 176 ms, Memory: 17656 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        System.out.println(str+"??!");
+        Scanner in = new Scanner(System.in);
+        
+        String lastString = "??!";
+        
+        String s = in.next();
+        System.out.println(s + lastString);
     }
 }
