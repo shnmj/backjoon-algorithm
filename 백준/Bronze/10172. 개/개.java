8c1d9b409c48276536c13b71bdc239846c617ce8
Shin Min Jae <163399297+shnmj@users.noreply.github.com>
2024-11-10 18:52:23 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13956 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -1,10 +1,26 @@
-public class Main{
-    
-    public static void main(String[] args){
-        System.out.println("|\\_/|");
-        System.out.println("|q p|   /}");
-        System.out.println("( 0 )\"\"\"\\");
-        System.out.println("|\"^\"`    |");
-        System.out.println("||_/=\\\\__|");
-    }
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");     
+        
+        
+/* 그 외의 방법
+		System.out.print(
+        "|\\_/|\n" + 
+		"|q p|   /}\n" + 
+		"( 0 )\"\"\"\\\n" + 
+		"|\"^\"`    |\n" + 
+		"||_/=\\\\__|");
+                         
+		System.out.printf("%s", "|\\_/|\n
+  		|q p|   /}\n
+ 		( 0 )\"\"\"\\\n
+		|\"^\"`    |\n
+		||_/=\\\\__|");
+        
+*/
+	}
 }
